b9920413bf35eca36201aa1005a0838fc6af52a4
Ehouarn Perret <ehouarn.perret@outlook.com>
2015-12-31 18:38:17 +0800

    First challenge...

---
+++ EhouarnPerret.CSharp.HackerRank/EhouarnPerret.CSharp.HackerRank.Sandbox/30DaysOfCode/Day0PrintHelloWorld.cs
@@ -0,0 +1,20 @@
+﻿// File: Day0PrintHelloWorld.cs
+// Author: Ehouarn Perret
+// Email: ehouarn.perret@art-logics.com
+// Company: ART logics
+// Website: http://www.art-logics.com
+// Copyright: ART logics © All Rights Reserved.
+using System;
+
+namespace EhouarnPerret.CSharp.HackerRank.Sandbox.DaysOfCode
+{
+    public static class Solution
+    {
+        public static void Main(params String[] arguments)
+        {
+            Console.WriteLine(@"Hello World.");
+            Console.WriteLine(@"Welcome to 30 Days of Code.");
+        }
+    }
+}
+
