79387653db778fd5d0ec80044d9494e70a1c84fc
Ehouarn Perret <ehouarn.perret@outlook.com>
2016-01-02 22:18:44 +0800

    2nd day of code on Hacker Rank... easy peasy

---
+++ EhouarnPerret.CSharp.HackerRank/EhouarnPerret.CSharp.HackerRank.Sandbox/30DaysOfCode/Day2ArithmeticSolution.cs
@@ -0,0 +1,38 @@
+﻿// File: Day2Solution.cs
+// Author: Ehouarn Perret
+// Email: ehouarn.perret@art-logics.com
+// Company: ART logics
+// Website: http://www.art-logics.com
+// Copyright: ART logics © All Rights Reserved.
+using System;
+
+namespace EhouarnPerret.CSharp.HackerRank.Sandbox.DaysOfCode
+{
+    public static class Day2ArithmeticSolution
+    {
+        public static void Main(params String[] arguments)
+        {
+            // Original price
+            var originalPrice = Convert.ToDouble(Console.ReadLine());
+
+            // Original Price Off Percentage
+            var originalPriceOffPercentage = Convert.ToInt32(Console.ReadLine());
+
+            // Original Price Tax Percentage
+            var originalPriceTaxPercentage = Convert.ToInt32(Console.ReadLine());
+
+            // Tip 
+            var tip = originalPrice * originalPriceOffPercentage / 100;
+
+            // Tax
+            var tax = originalPriceTaxPercentage * originalPrice / 100;
+
+            var finalPrice = originalPrice + tip + tax;
+
+            var mealFinalPrice = Math.Round(finalPrice);
+
+            Console.WriteLine(@"The final price of the meal is $" + mealFinalPrice + ".");
+        }
+    }
+}
+
