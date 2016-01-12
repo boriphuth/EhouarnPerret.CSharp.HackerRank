9d250950e4b02a97eb4d1341adb75b30c35dfec4
Ehouarn Perret <ehouarn.perret@outlook.com>
2016-01-12 23:27:42 +0800

    Day of code 12... C# Solution

---
+++ EhouarnPerret.CSharp.HackerRank/EhouarnPerret.CSharp.HackerRank.Sandbox/30DaysOfCode/Day12Inheritance/Student.cs
@@ -0,0 +1,55 @@
+﻿//
+// Student.cs
+//
+// Author:
+//       Ehouarn Perret <ehouarn.perret@outlook.com>
+//
+// Copyright (c) 2016 Ehouarn Perret
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+
+namespace EhouarnPerret.CSharp.HackerRank.Sandbox.DaysOfCode.Day12Inheritance
+{
+    public class Student
+    {
+        public Student(String firstName, String lastName, UInt32 phone)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this._phone = phone;
+        }
+
+        protected String firstName;
+        public String FirstName => this.firstName;
+
+        protected String lastName;
+        public String LastName => this.lastName;
+
+        private UInt32 _phone;
+        public UInt32 Phone => this._phone;
+
+        public void Display()
+        {
+            Console.WriteLine(String.Format(@"First Name:{0}{1}Last Name: {2}{1}Phone: {3}", this.firstName, Environment.NewLine, this.lastName, this._phone));
+        }
+    }
+
+}
+
